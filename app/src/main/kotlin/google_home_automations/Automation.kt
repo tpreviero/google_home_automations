4e5acc4d2e4f81110773a81988c1fed8af14650f
Tommaso Previero <t.previero@gmail.com>
2024-06-12 13:44:49 +0200

    Move automations definitions

--- app/src/main/kotlin/google_home_automations/Automation.kt
+++ app/src/main/kotlin/google_home_automations/Automation.kt
@@ -7,6 +7,15 @@ data class Automation(
     val actions: List<Actions>
 )
 
+val single = Action.entries.flatMap { action -> rollerShutters.map {"${action.emojify()} ${it.device} ${it.room}" to it.automation(action) } }
+val roomsAutomations = Action.entries.flatMap { action -> rollerShutters.groupBy(RollerShutter::room).map { (room, rs) -> "${action.emojify()} $room" to rs.automation(action) } }
+val stopAll = Action.entries.map { action -> "${action.emojify()} all" to rollerShutters.automation(action) {
+    listOf(Starters.Scheduled(
+        Starters.Scheduled.Hours._22,
+        Starters.Scheduled.Minutes._30, Starters.Scheduled.Weekdays.entries.toSet())).filter { action == Action.Stop }.toSet()
+}}
+val groups = Action.entries.flatMap { action -> rollerShutters.groups().map { "${action.emojify()} ${it.key}" to it.value.automation(action, it.key) } }
+
 fun RollerShutter.automation(action: Action) = Automation(
     "$action $device $room roller shutter",
     "Automation to ${action.toString().lowercase()} $device roller shutter for room $room",
@@ -30,6 +39,12 @@ fun List<RollerShutter>.automation(action: Action, group: String, additionalStar
         starters(action, group) + additionalStarters(),
         actions(action)
     )
+}
+
+private fun List<RollerShutter>.groups(): Map<String, List<RollerShutter>> {
+    return flatMap { rs -> rs.groups.map { Pair(it, rs) } }
+        .groupBy { it.first }
+        .mapValues { it.value.map { it.second } }
 }
 
 private fun List<RollerShutter>.rooms(): String = map { it.room }.toSet().joinToString(", ")
--- app/src/main/kotlin/google_home_automations/GoogleHomeAutomations.kt
+++ app/src/main/kotlin/google_home_automations/GoogleHomeAutomations.kt
@@ -1,14 +1,16 @@
 package google_home_automations
 
-import google_home_automations.Action.Stop
-import google_home_automations.Starters.Scheduled.Hours._22
-import google_home_automations.Starters.Scheduled.Minutes._30
-import google_home_automations.Starters.Scheduled.Weekdays
 import java.io.File
 
 val DRY_RUN = System.getenv("DRY_RUN").toBoolean()
 val HOUSE_ID: String get() = System.getenv("HOUSE_ID")
 val GOOGLE_AUTH_TOKEN: String get() = System.getenv("GOOGLE_AUTH_TOKEN")
+
+val automations: List<Pair<String, Automation>> =
+    single +
+    roomsAutomations +
+    groups +
+    stopAll
 
 fun main(args: Array<String>) {
 
@@ -19,26 +21,12 @@ fun main(args: Array<String>) {
         }
     }
 
-    val automations =
-        Action.entries.flatMap { action -> rollerShutters.map { "${action.emojify()} ${it.device} ${it.room}" to it.automation(action) } } +
-        Action.entries.flatMap { action -> rollerShutters.groupBy(RollerShutter::room).map { (room, rs) -> "${action.emojify()} $room" to rs.automation(action) } } +
-        Action.entries.map { action -> "${action.emojify()} all" to rollerShutters.automation(action) {
-            listOf(Starters.Scheduled(_22, _30, Weekdays.entries.toSet())).filter { action == Stop }.toSet()
-        }} +
-        Action.entries.flatMap { action -> rollerShutters.groups().map { "${action.emojify()} ${it.key}" to it.value.automation(action, it.key) } }
-
     automations.forEach {
         File("${args[0]}/${it.first}.yml").writeBytes(it.second.yaml().toByteArray())
         if (!DRY_RUN) {
             save(it.second, "${args[0]}/automations-ids")
         }
     }
-}
-
-private fun List<RollerShutter>.groups(): Map<String, List<RollerShutter>> {
-    return flatMap { rs -> rs.groups.map { Pair(it, rs) } }
-        .groupBy { it.first }
-        .mapValues { it.value.map { it.second } }
 }
 
 fun save(automation: Automation, idFile: String) {
